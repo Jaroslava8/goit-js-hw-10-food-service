0934448f17ce2a6a17962a9ec3044e9c9745007d
Jaroslava8 <jaroslava.moroziuk@gmail.com>
2021-10-06 20:16:24 +0100

    functions added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './sass/main.scss';
+import menuCards from './menu.json';
+import menuItems from './templates/menu-items.hbs';
+
+const menuList = document.querySelector('.js-menu');
+menuList.insertAdjacentHTML('beforeend', createMenuCards(menuCards));
+function createMenuCards(menuCards){
+    return menuItems(menuCards);
+}
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+const buttonSwitch = document.querySelector('#theme-switch-toggle');
+buttonSwitch.addEventListener('change', switchTheme);
+
+function switchTheme({target:{checked}}){
+    checked ? toggleTheme(Theme.DARK, Theme.LIGHT): toggleTheme(Theme.LIGHT, Theme.DARK);
+}
+function toggleTheme(add, rem){
+    document.body.classList.add(add);
+    document.body.classList.remove(rem);
+    localStorage.setItem('Theme', add);
+
+}
